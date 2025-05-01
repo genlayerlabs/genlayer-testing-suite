358b0919ec965236f0131dead60b37bb1667e263
Daniel Rojas <danielrojasc888@gmail.com>
2025-05-01 17:51:54 -0500

    fix: only create client when call

--- gltest/glchain/client.py
+++ gltest/glchain/client.py
@@ -4,15 +4,12 @@ from .account import default_account
 from functools import lru_cache
 from gltest.plugin_config import get_rpc_url
 
-# Create the client
-gl_client = create_client(
-    chain=localnet, account=default_account, endpoint=get_rpc_url()
-)
-
 
 @lru_cache(maxsize=1)
 def get_gl_client():
     """
     Get the GenLayer client instance.
     """
-    return gl_client
+    return create_client(
+        chain=localnet, account=default_account, endpoint=get_rpc_url()
+    )
