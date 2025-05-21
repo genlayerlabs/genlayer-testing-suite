00b3b520e2ec0bca65f015212182faa7532c3f5e
Daniel Rojas <danielrojasc888@gmail.com>
2025-05-21 11:25:30 -0500

    feat: updated and added tests

--- tests/examples/tests/test_multi_file_contract_legacy.py
+++ tests/examples/tests/test_multi_file_contract_legacy.py
@@ -2,10 +2,10 @@ from gltest import get_contract_factory
 from gltest.assertions import tx_execution_succeeded
 
 
-def test_multi_file_contract():
-    # Multi file contracts are considered if they are defined in a __init__.py file
-    # Deploy Contract, it will deploy other.py as well
-    factory = get_contract_factory("MultiFileContract")
+def test_multi_file_contract_legacy():
+    # Multi file contracts are considered if they are defined in a __init__.gpy file
+    # Deploy Contract, it will deploy other.gpy as well
+    factory = get_contract_factory("MultiFileContractLegacy")
     contract = factory.deploy(args=[])
 
     wait_response = contract.wait(args=[])
