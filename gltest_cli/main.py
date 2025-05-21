fd524be6c8a23c718492c56b2c4259f9fc8c5941
Daniel Rojas <danielrojasc888@gmail.com>
2025-05-21 17:57:57 -0500

    feat: add user config check on gltest command

--- gltest_cli/main.py
+++ gltest_cli/main.py
@@ -1,5 +1,13 @@
 import pytest
+from gltest.logging import logger
+from gltest.config import check_user_config, load_user_config
 
 
 def main():
-    return pytest.main()
+    if not check_user_config():
+        logger.error("gltest.config.yaml not found in the current directory")
+        return
+
+    config = load_user_config("gltest.config.yaml")
+    print(config)
+    # return pytest.main()
