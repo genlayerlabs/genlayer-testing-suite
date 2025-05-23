16d4e8159f4de7e466298438ec97f11063f41229
Daniel Rojas <danielrojasc888@gmail.com>
2025-05-23 18:20:31 -0500

    fix: workflow and update test

--- tests/gltest/artifact/test_contract_definition.py
+++ tests/gltest/artifact/test_contract_definition.py
@@ -9,13 +9,13 @@ from pathlib import Path
 
 def test_single_file():
     general_config = get_general_config()
-    general_config.set_contracts_dir(Path("."))
+    general_config.set_contracts_dir(Path("tests"))
     contract_definition = find_contract_definition("PredictionMarket")
 
     assert contract_definition.contract_name == "PredictionMarket"
 
     # Assert complete contract definition
-    expected_main_file_path = Path("examples/contracts/football_prediction_market.py")
+    expected_main_file_path = Path("tests/examples/contracts/football_prediction_market.py")
     expected_runner_file_path = None
     contract_code = compute_contract_code(
         expected_main_file_path, expected_runner_file_path
@@ -23,22 +23,22 @@ def test_single_file():
     assert contract_definition.contract_code == contract_code
     assert (
         str(contract_definition.main_file_path)
-        == "examples/contracts/football_prediction_market.py"
+        == "tests/examples/contracts/football_prediction_market.py"
     )
     assert contract_definition.runner_file_path is None
 
 
 def test_multiple_files():
     general_config = get_general_config()
-    general_config.set_contracts_dir(Path("."))
+    general_config.set_contracts_dir(Path("tests"))
     contract_definition = find_contract_definition("MultiFileContract")
 
     assert contract_definition.contract_name == "MultiFileContract"
 
     # Assert complete contract definition
-    expected_main_file_path = Path("examples/contracts/multi_file_contract/__init__.py")
+    expected_main_file_path = Path("tests/examples/contracts/multi_file_contract/__init__.py")
     expected_runner_file_path = Path(
-        "examples/contracts/multi_file_contract/runner.json"
+        "tests/examples/contracts/multi_file_contract/runner.json"
     )
     assert contract_definition.main_file_path == expected_main_file_path
     assert contract_definition.runner_file_path == expected_runner_file_path
@@ -50,12 +50,12 @@ def test_multiple_files():
 
 def test_single_file_legacy():
     general_config = get_general_config()
-    general_config.set_contracts_dir(Path("."))
+    general_config.set_contracts_dir(Path("tests"))
     contract_definition = find_contract_definition("StorageLegacy")
 
     # Assert complete contract definition
     assert contract_definition.contract_name == "StorageLegacy"
-    expected_main_file_path = Path("examples/contracts/storage_legacy.gpy")
+    expected_main_file_path = Path("tests/examples/contracts/storage_legacy.gpy")
     expected_runner_file_path = None
     contract_code = compute_contract_code(
         expected_main_file_path, expected_runner_file_path
@@ -63,23 +63,23 @@ def test_single_file_legacy():
     assert contract_definition.contract_code == contract_code
     assert (
         str(contract_definition.main_file_path)
-        == "examples/contracts/storage_legacy.gpy"
+        == "tests/examples/contracts/storage_legacy.gpy"
     )
     assert contract_definition.runner_file_path is None
 
 
 def test_multiple_files_legacy():
     general_config = get_general_config()
-    general_config.set_contracts_dir(Path("."))
+    general_config.set_contracts_dir(Path("tests"))
     contract_definition = find_contract_definition("MultiFileContractLegacy")
 
     # Assert complete contract definition
     assert contract_definition.contract_name == "MultiFileContractLegacy"
     expected_main_file_path = Path(
-        "examples/contracts/multi_file_contract_legacy/__init__.gpy"
+        "tests/examples/contracts/multi_file_contract_legacy/__init__.gpy"
     )
     expected_runner_file_path = Path(
-        "examples/contracts/multi_file_contract_legacy/runner.json"
+        "tests/examples/contracts/multi_file_contract_legacy/runner.json"
     )
     assert contract_definition.main_file_path == expected_main_file_path
     assert contract_definition.runner_file_path == expected_runner_file_path
@@ -91,7 +91,7 @@ def test_multiple_files_legacy():
 
 def test_class_is_not_intelligent_contract():
     general_config = get_general_config()
-    general_config.set_contracts_dir(Path("."))
+    general_config.set_contracts_dir(Path("tests"))
 
     with pytest.raises(FileNotFoundError):
         _ = find_contract_definition("NotICContract")
