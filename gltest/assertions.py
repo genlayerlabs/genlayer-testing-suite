edfb642b9abb118a65428aff3c3848bed224479c
Daniel Rojas <danielrojasc888@gmail.com>
2025-06-03 13:27:05 -0500

    feat: update execution succeded assertion

--- gltest/assertions.py
+++ gltest/assertions.py
@@ -6,9 +6,9 @@ def tx_execution_succeeded(result: GenLayerTransaction) -> bool:
         return False
     if "leader_receipt" not in result["consensus_data"]:
         return False
-    if "execution_result" not in result["consensus_data"]["leader_receipt"]:
+    if "execution_result" not in result["consensus_data"]["leader_receipt"][0]:
         return False
-    execution_result = result["consensus_data"]["leader_receipt"]["execution_result"]
+    execution_result = result["consensus_data"]["leader_receipt"][0]["execution_result"]
     return execution_result == "SUCCESS"
 
 
