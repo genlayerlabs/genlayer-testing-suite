4de823ce5eea23fc211165adb4deea4570964b6e
Daniel Rojas <danielrojasc888@gmail.com>
2025-06-04 22:14:02 -0500

    fix: client and add typing

--- gltest/glchain/client.py
+++ gltest/glchain/client.py
@@ -11,10 +11,9 @@ def get_gl_client():
     Get the GenLayer client instance.
     """
     general_config = get_general_config()
-    selected_network = general_config.get_network_name()
-    chain = general_config.get_chain(selected_network)
+    chain = general_config.get_chain()
     default_account = get_default_account()
-    endpoint = general_config.get_rpc_url(selected_network)
+    endpoint = general_config.get_rpc_url()
     return create_client(
         chain=chain,
         account=default_account,
