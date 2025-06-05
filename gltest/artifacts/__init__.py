db6d019825dc440f94fc218d8165699a35ee9bcd
Daniel Rojas <danielrojasc888@gmail.com>
2025-06-05 00:14:50 -0500

    apply black

--- gltest/artifacts/__init__.py
+++ gltest/artifacts/__init__.py
@@ -1,3 +1,6 @@
-from .contract import find_contract_definition_from_name, find_contract_definition_from_path
+from .contract import (
+    find_contract_definition_from_name,
+    find_contract_definition_from_path,
+)
 
 __all__ = ["find_contract_definition_from_name", "find_contract_definition_from_path"]
