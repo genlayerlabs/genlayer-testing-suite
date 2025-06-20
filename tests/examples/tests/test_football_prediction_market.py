29928aa1f8de01f90fd4f0779b5981ba3ff8e513
Daniel Rojas <danielrojasc888@gmail.com>
2025-06-20 02:17:58 -0500

    test: update tests to use mocks and validator setup

--- tests/examples/tests/test_football_prediction_market.py
+++ tests/examples/tests/test_football_prediction_market.py
@@ -1,8 +1,27 @@
 from gltest import get_contract_factory
 from gltest.assertions import tx_execution_succeeded
+import json
 
 
-def test_football_prediction_market():
+def test_football_prediction_market(setup_validators):
+
+    # Setup validators with mock response
+    team_1 = "Georgia"
+    team_2 = "Portugal"
+    score = "2:0"
+    winner = 1
+    mock_response = {
+        "response": {
+            f"Team 1: {team_1}\nTeam 2: {team_2}": json.dumps(
+                {
+                    "score": score,
+                    "winner": winner,
+                }
+            ),
+        }
+    }
+    setup_validators(mock_response)
+
     # Deploy Contract
     factory = get_contract_factory("PredictionMarket")
     contract = factory.deploy(args=["2024-06-26", "Georgia", "Portugal"])
