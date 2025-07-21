142001789b8d1895a58fc12964fe2b25d4b47837
Daniel Rojas <danielrojasc888@gmail.com>
2025-07-21 14:58:59 -0500

    feat: add studionet and testnet_asimov as pre configured networks

--- gltest_cli/config/constants.py
+++ gltest_cli/config/constants.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 GLTEST_CONFIG_FILE = "gltest.config.yaml"
 DEFAULT_NETWORK = "localnet"
+PRECONFIGURED_NETWORKS = ["localnet", "studionet", "testnet_asimov"]
 DEFAULT_RPC_URL = SIMULATOR_JSON_RPC_URL
 DEFAULT_ENVIRONMENT = ".env"
 DEFAULT_CONTRACTS_DIR = Path("contracts")
